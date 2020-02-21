f93a4ca1211951d28a24d775893231dbe71f0664
Bruno Perraudin <b.perraudin@catie.fr>
2020-02-21 17:37:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zest_Adapter_Shield
+
+Zest_Adapter_Shield library for Mbed OS
+
+<!-- Describe `ZestAdapterShield` library here -->
---
+++ zest_adapter_shield/zest_adapter_shield.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+#define CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+
+namespace sixtron {
+
+class ZestAdapterShield
+{
+public:
+    ZestAdapterShield();
+};
+
+} // namespace sixtron
+
+#endif // CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+
