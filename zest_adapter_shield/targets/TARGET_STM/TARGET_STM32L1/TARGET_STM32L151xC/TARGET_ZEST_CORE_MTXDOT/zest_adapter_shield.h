e27534558571774596698ab1250dbba4bf622ab8
Pierre-Marie Ancèle <pm.ancele@catie.fr>
2024-03-25 11:14:09 +0000

    Add Zest_Core_MTXDOT support

---
+++ zest_adapter_shield/targets/TARGET_STM/TARGET_STM32L1/TARGET_STM32L151xC/TARGET_ZEST_CORE_MTXDOT/zest_adapter_shield.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+#define CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+
+// Arduino connector namings
+#define A0  PA_5
+#define A1  NC
+#define A2  NC
+#define A3  NC
+#define A4  PB_9
+#define A5  PB_8
+#define D0  PA_9
+#define D1  PA_10
+#define D2  PA_2 // If R22 is mounted and R20 not mounted
+#define D3  PB_2
+#define D4  PA_12 // If R16 is mounted and R18 not mounted (button disconnected)
+#define D5  NC
+#define D6  NC
+#define D7  PA_11 // If R15 is mounted and R17 not mounted (LED disconnected)
+#define D8  NC
+#define D9  PB_0
+#define D10 PB_12
+#define D11 PB_15
+#define D12 PB_14
+#define D13 PB_13
+#define D14 PB_9
+#define D15 PB_8
+
+#endif // CATIE_SIXTRON_ZESTADAPTERSHIELD_H_
+
